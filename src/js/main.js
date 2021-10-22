02e8f2f70a3a86090a573c0a3bab0c891b1b2415
Jadson-Js <jadson20051965@gmail.com>
2021-10-21 22:18:56 -0300

    Toggle close when click on link

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
 const toggleButton = document.querySelector("#toggleButton")
 const toggleMenu = document.querySelector("#toggleMenu")
 
+const nav__link1 = document.querySelector(".nav__link1")
+const nav__link2 = document.querySelector(".nav__link2")
+const nav__link3 = document.querySelector(".nav__link3")
+const nav__link4 = document.querySelector(".nav__link4")
+const nav__link5 = document.querySelector(".nav__link5")
+const nav__link6 = document.querySelector(".nav__link6")
+
 toggleButton.addEventListener('click', () => {
     if (toggleMenu.classList[1] == "toggle--close") {
         toggleMenu.classList.remove("toggle--close")
@@ -9,4 +16,34 @@ toggleButton.addEventListener('click', () => {
         toggleMenu.classList.remove("toggle--open")
         toggleMenu.classList.add('toggle--close')
     }
+})
+
+nav__link1.addEventListener('click', () => {
+    toggleMenu.classList.remove("toggle--open")
+    toggleMenu.classList.add('toggle--close')
+})
+
+nav__link2.addEventListener('click', () => {
+    toggleMenu.classList.remove("toggle--open")
+    toggleMenu.classList.add('toggle--close')
+})
+
+nav__link3.addEventListener('click', () => {
+    toggleMenu.classList.remove("toggle--open")
+    toggleMenu.classList.add('toggle--close')
+})
+
+nav__link4.addEventListener('click', () => {
+    toggleMenu.classList.remove("toggle--open")
+    toggleMenu.classList.add('toggle--close')
+})
+
+nav__link5.addEventListener('click', () => {
+    toggleMenu.classList.remove("toggle--open")
+    toggleMenu.classList.add('toggle--close')
+})
+
+nav__link6.addEventListener('click', () => {
+    toggleMenu.classList.remove("toggle--open")
+    toggleMenu.classList.add('toggle--close')
 })
